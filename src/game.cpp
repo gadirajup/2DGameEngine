8ecc37a85bf68a632a3633f21621e6905829837a
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2020-03-31 04:48:52 +0000

    Init Game Class

---
+++ src/game.cpp
@@ -0,0 +1,17 @@
+#include "game.hpp"
+
+Game::Game() {}
+
+Game::~Game() {}
+
+void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {}
+
+void Game::handleEvents() {}
+
+void Game::update() {}
+
+void Game::render() {}
+
+void Game::clean() {}
+
+bool Game::running() { return isRunning; }
